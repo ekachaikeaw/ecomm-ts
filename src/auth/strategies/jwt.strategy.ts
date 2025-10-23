2bec49dd7e16f0b151fa74f6be611d95917609b5
DNOTE\User <ekachai.keawman@gmail.com>
2025-10-23 09:32:42 +0700

    fix: fix test

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -12,7 +12,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: any) {
+  async validate(payload: { sub: string; name: string }) {
     if (!payload) {
       throw new UnauthorizedException('Invalid token');
     }
